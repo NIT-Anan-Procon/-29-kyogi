0db5cd23f658fcfc5097310be0ee7d339f5308a8
YumaWatabe <1157591@st.anan-nct.ac.jp>
2017-05-16 22:46:54 +0900

    Edited Vector.c and head/vector.h

---
+++ Vector.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "head/coord.h"
+
+typedef struct {
+  int x;
+  int y;
+}Vector;
+
+Vector find_vector(Coord a, Coord b){
+  Vector v;
+  v.x = b.x - a.x;
+  v.y = b.y - a.y;
+  return v;
+}
+
+double find_angle(Vector a, Vector b){
+	double inner_product, rad;
+	inner_product = ((a.x * b.x) + (a.y * b.y)) /
+				(sqrt((a.x*a.x)+(a.y*a.y)) * sqrt((b.x*b.x)+(b.y*b.y)));
+	rad = M_PI - acos(inner_product);
+  return rad;
+}
