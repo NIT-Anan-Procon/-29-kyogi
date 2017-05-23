5c7db9c9e1e11f67d69d0b1b3dfc95124255183a
Ayato Hashimoto <1157027@st.anan-nct.ac.jp>
2017-05-23 19:20:58 +0900

    add puzzle.c/ solve method

--- head/puzzle.h
+++ head/puzzle.h
@@ -1,3 +1,8 @@
 #include <stdio.h>
 
+#include "head/polygon.h"
+
+#define NEW_VECTOR(a,b) ((Vector){a,b})
+#define NEW_COORD(a,b) ((Coord){a,b})
+
 int solve(void);
--- main.c
+++ main.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 
-typedef struct{
-	int x;
-	int y;
-}Coord;
-
-typedef struct{
-	int x;
-	int y;
-}Vector;
-
-typedef struct{
-	Coord vertex[16];
-	int vertex_number;
-	int side[16];
-	double angle[16]; 
-}Polygon;
+#include "head/puzzle.h"
 
 int main(void){
-
+	solve();
 
 	return 0;
 }
